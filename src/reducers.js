cfeefecfb90deb578adfe57f6784012998dc1a64
Dima Paloskin <dima@paloskin.me>
2017-09-13 21:17:57 +0300

    it was mutable. I overlooked it

--- src/reducers.js
+++ src/reducers.js
@@ -3,8 +3,10 @@ import { ADD_TODO } from './actions'
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      state.push(action.payload)
-      return state
+      return [
+        ...state,
+        action.payload
+      ]
     default:
       return state
   }
